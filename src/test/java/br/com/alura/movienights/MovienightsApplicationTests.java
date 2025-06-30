94e3965ea7026de4c9d11b96ce0724c44065aff8
Aoife <imatt.lino@gmail.com>
2025-06-29 21:06:25 -0300

    criando classe Principal

--- src/test/java/br/com/alura/movienights/MovienightsApplicationTests.java
+++ src/test/java/br/com/alura/movienights/MovienightsApplicationTests.java
@@ -1,17 +1,10 @@
 package br.com.alura.movienights;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import br.com.alura.movienights.model.DadosEpisodio;
-import br.com.alura.movienights.model.DadosSerie;
-import br.com.alura.movienights.model.DadosTemporada;
-import br.com.alura.movienights.service.ConsumoApi;
-import br.com.alura.movienights.service.ConverteDados;
+import br.com.alura.movienights.principal.Principal;
 
 @SpringBootTest
 class MovienightsApplicationTests implements CommandLineRunner {
@@ -22,23 +15,16 @@ class MovienightsApplicationTests implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=74c3f45b");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=74c3f45b");
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 
-		List<DadosTemporada> temporadas = new ArrayList<>();
+		// List<DadosTemporada> temporadas = new ArrayList<>();
 
-		for (int i = 1; i<=dados.totalTemporadas(); i++) {
-			json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=74c3f45b");
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-		temporadas.forEach(System.out::println);
+		// for (int i = 1; i<=dados.totalTemporadas(); i++) {
+		// 	json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=74c3f45b");
+		// 	DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+		// 	temporadas.add(dadosTemporada);
+		// }
+		// temporadas.forEach(System.out::println);
 	}
 }
--- src/test/java/br/com/alura/movienights/principal/Principal.java
+++ src/test/java/br/com/alura/movienights/principal/Principal.java
@@ -1,5 +1,26 @@
 package br.com.alura.movienights.principal;
 
+import java.util.Scanner;
+
+import br.com.alura.movienights.model.DadosSerie;
+import br.com.alura.movienights.service.ConsumoApi;
+import br.com.alura.movienights.service.ConverteDados;
+
 public class Principal {
 
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
+
+
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=74c3f45b";
+
+    public void exibeMenu(){
+        System.out.println("Digite o nome da série");
+        var nomeSerie = leitura.nextLine();
+		var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+    }
 }
