14fa2c0e8e6b592969b0d48ddad36a450936c0d1
Aoife <imatt.lino@gmail.com>
2025-06-29 21:05:29 -0300

    criando classe Principal

--- src/test/java/br/com/alura/movienights/model/DadosTemporada.java
+++ src/test/java/br/com/alura/movienights/model/DadosTemporada.java
@@ -1,5 +1,12 @@
 package br.com.alura.movienights.model;
 
-public record DadosTemporada() {
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosTemporada(@JsonAlias("Season") Integer numero,
+                             @JsonAlias("Episodes") List<DadosEpisodio> episodios) {
 
 }
