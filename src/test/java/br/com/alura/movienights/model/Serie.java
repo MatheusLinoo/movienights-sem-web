930c362afde496599716ad7b64fcf3bd6bddda6c
Aoife <imatt.lino@gmail.com>
2025-07-07 17:30:40 -0300

    modelando categorias e series

--- src/test/java/br/com/alura/movienights/model/Serie.java
+++ src/test/java/br/com/alura/movienights/model/Serie.java
@@ -1,5 +1,7 @@
 package br.com.alura.movienights.model;
 
+import java.util.OptionalDouble;
+
 public class Serie {
     private String titulo;
     private Integer totalTemporadas;
@@ -8,4 +10,14 @@ public class Serie {
     private String atores;
     private String poster;
     private String sinopse;
+
+    public Serie (DadosSerie dadosSerie) {
+        this.titulo = dadosSerie.titulo();
+        this.totalTemporadas = dadosSerie.totalTemporadas();
+        this.avaliacao = OptionalDouble.of(Double.valueOf(dadosSerie.avaliacao())).orElse(0);
+        this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
+        this.atores = dadosSerie.atores();
+        this.poster = dadosSerie.poster();
+        this.sinopse = dadosSerie.sinopse();
+    }
 }
